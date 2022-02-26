0452794a982510f0ba63bdc9b81ef0c63dd486bf
Fauzi Kurniawan <kirigaya.kurniawan@gmail.com>
2022-02-27 00:19:02 +0700

    Git commit movies-catalogue

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'f3474346f2ab0e7470b39b34dc119688',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
